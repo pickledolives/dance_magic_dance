bcdecf8496baccce5ac79520c6e45b176e817207
Oliver Thamm <pickledolives@users.noreply.github.com>
2019-03-20 10:31:03 +0100

    remove magic number in RenderPieceTile

--- app/views/cli/render_piece_tile.rb
+++ app/views/cli/render_piece_tile.rb
@@ -2,11 +2,13 @@
 
 module RenderPieceTile
 
+  ROWS_PER_TILE = 3
+
   def self.call(piece)
     tile = MakePieceTile.call(piece)
     puts "    n     "
     puts " #{'-' * 8} "
-    3.times { |i| puts "#{i == 1 ? 'w' : ' '}|#{tile[i].join}|#{i == 1 ? 'e' : ' '}" }
+    ROWS_PER_TILE.times { |i| puts "#{i == 1 ? 'w' : ' '}|#{tile[i].join}|#{i == 1 ? 'e' : ' '}" }
     puts " #{'-' * 8} "
     puts "    s     "
   end
